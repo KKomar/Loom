6f6c0bdca88d52118ec107af02952cbcc7f52c57
Iliana <bonobono000011@gmail.com>
2018-03-01 20:55:14 +0200

    Adaptive design

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
+    $('.header-menu').slicknav({
+        prependTo: '.mobile-menu'
+    });
+
+    // $('.slicknav_btn').addClass('clearfix');
+    $('.slicknav_btn').append('<i class="fa fa-bars" aria-hidden="true"></i>')
+});
+
+$(document).ready(function () {
     $('.slider-wrap').slick({
         dots: false
     });
